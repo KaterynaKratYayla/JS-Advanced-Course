7b5f0e102479951baf0e1e5b02798fe01596ce0e
KaterynaKratYayla <kkrat@arktur.ua>
2020-09-11 11:33:52 +0300

    Add existing file

---
+++ js_canvas.js
@@ -0,0 +1,6 @@
+var canvas = document.getElementById('cv');
+var ctx = canvas.getContext('2d');
+ctx.fillStyle = 'red';
+ctx.fillRect(100, 50, 150, 75);
+ctx.fillStyle = 'blue';
+ctx.fillRect(150, 100, 100, 50);
